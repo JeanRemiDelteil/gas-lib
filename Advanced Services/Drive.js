9955a1a10c0bd9caad347e4cc15fce24954a29b4
JeanRemiDelteil <jeanremi.delteil@gmail.com>
2016-05-19 17:21:27 +0200

    Drive

--- Advanced Services/Drive.js
+++ Advanced Services/Drive.js
@@ -9,28 +9,78 @@ Drive.File.prototype.copyable = Boolean();
 Drive.File.prototype.editable = Boolean();
 
 
-/**
- *
- * @constructor
- *
- * @retun {Drive.FilesCollection}
- */
-Drive.Files = function () {};
+Drive.Files = {};
 
 /**
  * @param {Drive.File} resource
  * @param {string} field
  * @param {Object} [optionalArgs]
+ *
+ * @return {Drive.File}
  */
 Drive.Files.patch = function (resource, field, optionalArgs) {};
 
-
-Drive.FilesCollection = function () {};
-
+/**
+ * @param {Drive.File} resource
+ * @param {Blob} mediaData
+ * @param {Object} [optionalArgs]
+ *
+ * @return {Drive.File}
+ */
+Drive.Files.insert = function (resource, mediaData, optionalArgs) {};
 
 /**
  * @param {string} id
  *
  * @return {Drive.File}
  */
-Drive.FilesCollection.prototype.get = function (id) {};
+Drive.Files.get = function (id) {};
+
+//Drive.FilesCollection = function () {};
+
+
+/**
+ * @type {{
+ *  id: string,
+ *  kind: string
+ * }}
+ */
+Drive.PermissionId = {};
+
+/**
+ * @type {{
+ *  additionalRoles: Array<String>,
+ *  authKey: string,
+ *  domain: string,
+ *  emailAddress: string,
+ *  etag: string,
+ *  id: string,
+ *  kind: string,
+ *  name: string,
+ *  photoLink: string,
+ *  role: string,
+ *  selfLink: string,
+ *  type: string,
+ *  value: string,
+ *  withLink: boolean
+ * }}
+ */
+Drive.Permission = {};
+
+Drive.Permissions = function(){};
+
+/**
+ * @param {Drive.Permission} resource
+ * @param {string} fileId
+ * @param {Object} [optionalArgs]
+ *
+ * @return {Drive.Permission}
+ */
+Drive.Permissions.insert = function (resource, fileId, optionalArgs) {};
+
+/**
+ * @param {string} email
+ *
+ * @return {Drive.PermissionId}
+ */
+Drive.Permissions.getIdForEmail = function (email){};
